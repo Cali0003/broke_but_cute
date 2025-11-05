a472fba46898f8a1695df6e0367f405cd53692e1
Cali0003 <cali0003@stud.ek.dk>
2025-11-05 09:47:19 +0100

    Tekst_boxes

--- js/main.js
+++ js/main.js
@@ -22,6 +22,9 @@ function mouseOut() {
 
 function clickHead() {
   console.log("clickHead");
+
+  animateBoxes();
+
   document.querySelector(".info-text > h2").textContent = "Sælg ud";
   document.querySelector(".placeholder").textContent = "blah blah blah";
 
@@ -51,6 +54,9 @@ function mouseBff() {
 
 function clickBff() {
   console.log("clickHead");
+
+  animateBoxes();
+
   document.querySelector(".info-text > h2").textContent = "Sælg ud";
   document.querySelector(".placeholder").textContent = "blah blah blah";
 
@@ -85,6 +91,9 @@ function mouseKalender() {
 
 function clickKalender() {
   console.log("clickHead");
+
+  animateBoxes();
+
   document.querySelector(".info-text > h2").textContent = "Sælg ud";
   document.querySelector(".placeholder").textContent = "blah blah blah";
 
@@ -93,4 +102,26 @@ function clickKalender() {
 
   document.querySelector("#requirement").innerHTML = `<h3>wow</h3>
 <p>køb en masse lolern</p>`;
+}
+
+function animateBoxes() {
+  document.querySelector("#efficiency").classList.remove("hide");
+  document.querySelector("#efficiency").classList.add("fadeIn");
+  document.querySelector("#requirement").classList.remove("hide");
+  document.querySelector("#requirement").classList.add("fadeIn");
+
+  document
+    .querySelector("#requirement")
+    .addEventListener("animationend", cleanup);
+}
+
+function cleanup() {
+  console.log("cleanup");
+
+  document
+    .querySelector("#requirement")
+    .removeEventListener("animationend", cleanup);
+
+  document.querySelector("#efficiency").classList.remove("fadeIn");
+  document.querySelector("#requirement").classList.remove("fadeIn");
 }
